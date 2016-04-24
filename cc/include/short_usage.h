932ef040c0214e23ed26273a8e4aab26590e8a20
dgerenrot <dgerenrot@gmail.com>
2016-04-24 14:36:13 -0700

    Add usage headers for those who build on windows.

---
+++ cc/include/short_usage.h
@@ -0,0 +1,28 @@
+#ifndef __QHTML_SHORT_USAGE__
+#define __QHTML_SHORT_USAGE__
+#include <string>
+
+// GENERATED FILE! DO NOT EDIT!
+
+using namespace std;
+
+const string SHORT_USAGE = "\n"
+"\tUSAGE:\n"
+"\n"
+"\tqhtmlpg [OPTIONS] <TagPatternString>\n"
+"\n"
+"\tQuickly specify the structure of your HTML template on the command line!\n"
+"\tThe <TagPatternString> determines the nesting of the tags using square\n"
+"\tbrackets. \n"
+"\t\n"
+"\tThe correct doctype, html head and title tags are also included.\n"
+"\n"
+"\tFor example, if <TagPatternString> is '[]', then the output BODY contains\n"
+"\ta single pair of open/close DIV tags. Some other tags are also supported.\n"
+"\t\n"
+"\tThe resulting HTML template is printed to stdout.\n"
+"\t\n"
+"\tFor full usage info, specify the -u option.\n"
+;
+
+#endif
