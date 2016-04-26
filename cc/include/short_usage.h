205ed0bf61eb2f0eee76f4863a4b525029285a91
dgerenrot <dgerenrot@gmail.com>
2016-04-25 21:22:41 -0700

    Add header generation to visual studio project.

--- cc/include/short_usage.h
+++ cc/include/short_usage.h
@@ -1,28 +1,27 @@
 #ifndef __QHTML_SHORT_USAGE__
 #define __QHTML_SHORT_USAGE__
+
 #include <string>
 
 // GENERATED FILE! DO NOT EDIT!
 
-using namespace std;
-
-const string SHORT_USAGE = "\n"
-"\tUSAGE:\n"
-"\n"
-"\tqhtmlpg [OPTIONS] <TagPatternString>\n"
-"\n"
-"\tQuickly specify the structure of your HTML template on the command line!\n"
-"\tThe <TagPatternString> determines the nesting of the tags using square\n"
-"\tbrackets. \n"
-"\t\n"
-"\tThe correct doctype, html head and title tags are also included.\n"
-"\n"
-"\tFor example, if <TagPatternString> is '[]', then the output BODY contains\n"
-"\ta single pair of open/close DIV tags. Some other tags are also supported.\n"
-"\t\n"
-"\tThe resulting HTML template is printed to stdout.\n"
-"\t\n"
-"\tFor full usage info, specify the -u option.\n"
+const std::string SHORT_USAGE = 
+""
+"\tUSAGE:"
+""
+"\tqhtmlpg [OPTIONS] <TagPatternString>"
+""
+"\tQuickly specify the structure of your HTML template on the command line!"
+"\tThe <TagPatternString> determines the nesting of the tags using square"
+"\tbrackets. "
+"\t"
+"\tThe correct doctype, html head and title tags are also included."
+""
+"\tFor example, if <TagPatternString> is '[]', then the output BODY contains"
+"\ta single pair of open/close DIV tags. Some other tags are also supported."
+"\t"
+"\tThe resulting HTML template is printed to stdout."
+"\t"
+"\tFor full usage info, specify the -u option."
 ;
-
 #endif
